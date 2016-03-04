432870cd040cf6ab2c02bac4506ffeec1b1c359d
ariapahlavan <apahlavan1@utexas.edu>
2016-03-03 20:21:56 -0600

    >> Changed the way dictory file name was being passed to driver. >> Improved the NoSuchLadderExist class >> Added a simple Test.txt file >> Implemented the validateResult() method that returns true if the ladder satisfies the specified rules. >> Implemented the numDifferenceChar() method that does a char by char comparison and returns the count of dissimilar letters. >> Implemented the createGraph() method that creates a graph of all vertices (words) in the dictionary and adds to each vertex its appropriate edges.

--- Assignment4/src/Assignment4/Dictionary.java
+++ Assignment4/src/Assignment4/Dictionary.java
@@ -31,16 +31,50 @@ public class Dictionary {
 
 	}
 
+	//Setters and Getters
+	public ArrayList<String> getDictionary(){
+		return dictionary;
+	}
+
+	public void setDictionary(ArrayList<String> newDictionary){
+		dictionary = newDictionary;
+	}
+
 	/**
 	 * This method will create a graph containing every word in
 	 * dictionary as its Vertices and each vertex wil be connected
 	 * to all vertices that are only one distance apart.
 	 */
     private void createGraph () {
+	    String first = new String();
+	    String second = new String();
+
+	    //convert all words to vertices and fill up the graph list
 		for (String word : dictionary){
 
-			
+			Vertex newPhrase = new Vertex(word);
+			graph.add(newPhrase);
 		}
+
+	    int size = graph.size();
+	    //now connect the vertices with correct edges
+	    for (int i=0; i<size; i+=1) {
+
+		    Vertex vertex = graph.get(i);
+		    first.equals(vertex.getPhrase());
+		    for (int j = 0; j < size; j += 1) {
+
+			    Vertex edgeVertex = graph.get(j);
+			    second.equals(edgeVertex.getPhrase());
+
+			    if ( j != i ) {
+
+				    int distance = numDifferentChar(first, second);
+				    if ( distance == 1 )
+					    vertex.setEdges(edgeVertex);
+			    }
+		    }
+	    }
 	}
 
 
@@ -54,16 +88,40 @@ public class Dictionary {
 		return dictionary.contains(word);
 	}
 
-	//Setters and Getters
-	public ArrayList<String> getDictionary(){
-		return dictionary;
-	}
-	
-	public void setDictionary(ArrayList<String> newDictionary){
-		dictionary = newDictionary;
-	}
 
-	public BreadthFirstSearch(){
+	/**
+	 *
+	 */
+    public BreadthFirstSearch(){
 
 	}
+
+
+	/**
+	 * This method compares to strings character by character and counts the number
+	 * of character differences between the two input parameters.
+	 * @param s1
+	 * @param s2
+	 * @return the number of character difference between the two string parameters.
+	 */
+	private int numDifferentChar (String s1, String s2) {
+		//counter of the differing letters
+		int counter = 0;
+		char[] firstWord = s1.toCharArray();
+		char[] secondWord = s2.toCharArray();
+
+		// the size of the shortest string
+		// (more specifically character array).
+		int size = Math.min(s1.length(), s2.length());
+
+		// checking the letters one by one and
+		// counting the unlike chars
+		for (int i=0; i<size; i+=1 ){
+			if ( firstWord[i] != secondWord[i] )
+				counter += 1;
+		}
+
+		return counter;
+	}
+
 }
--- Assignment4/src/Assignment4/Vertex.java
+++ Assignment4/src/Assignment4/Vertex.java
@@ -22,5 +22,50 @@ public class Vertex {
         this.phrase = "";
     }
 
+    /**
+     * Custom constructor
+     * @param phrase
+     */
+    public Vertex (String phrase) {
+        this();
+        this.phrase = phrase;
+    }
 
+    //Getters and Setters
+
+    public String getPhrase() {
+        return phrase;
+    }
+
+    public ArrayList<Vertex> getEdges() {
+        return edges;
+    }
+
+    public Vertex getParent() {
+        return parent;
+    }
+
+    public boolean isWasChecked() {
+        return wasChecked;
+    }
+
+    public void setEdges(ArrayList<Vertex> edges) {
+        this.edges = edges;
+    }
+
+    public void setEdges(Vertex edge) {
+        this.edges.add(edge);
+    }
+
+    public void setParent(Vertex parent) {
+        this.parent = parent;
+    }
+
+    public void setWasChecked(boolean wasChecked) {
+        this.wasChecked = wasChecked;
+    }
+
+    public void setPhrase(String phrase) {
+        this.phrase = phrase;
+    }
 }

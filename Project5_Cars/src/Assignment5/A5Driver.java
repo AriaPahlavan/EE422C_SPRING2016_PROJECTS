cbf3cdf44276953b92d162cb492e3a33784e4ca8
ariapahlavan <apahlavan1@utexas.edu>
2016-04-04 19:07:55 -0500

    Major update: > painted the car body and the tires. > Fixed the flickerings after each repaint. > Implemented the car movement code.

--- Project5_Cars/src/Assignment5/A5Driver.java
+++ Project5_Cars/src/Assignment5/A5Driver.java
@@ -2,27 +2,92 @@ package Assignment5;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  * Project5_Cars
  * Created by Aria Pahlavan on Apr 2016.
  */
-public class A5Driver extends Applet {
+public class A5Driver extends Applet implements Runnable, KeyListener {
 
-    CarDrawer myCar;
+    private CarDrawer myCar;
+    private Graphics doubleG;
+    private Image image;
 
     @Override
     public void init() {
         this.setSize(600, 600);
+        addKeyListener(this);
     }
 
     @Override
     public void start() {
         myCar = new CarDrawer();
+        Thread thread = new Thread(this);
+        thread.start();
     }
+
+    @Override
+    public void run() {
+        while ( true ) {
+
+            myCar.setxCar(myCar.getxCar() + 1);
+            repaint();
+            try {
+                Thread.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    @Override
+    public void update(Graphics g) {
+        if ( image == null ) {
+            image = createImage(this.getWidth(), this.getHeight());
+            doubleG = image.getGraphics();
+        }
+
+        doubleG.setColor(getBackground());
+        doubleG.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+        doubleG.setColor(getForeground());
+        paint(doubleG);
+
+        g.drawImage(image, 0, 0, this);
+    }
+
 
     @Override
     public void paint(Graphics g) {
         myCar.paint(g);
+    }
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyChar()){
+            case KeyEvent.VK_A:
+            case KeyEvent.VK_LEFT:
+                myCar.moveLeft();
+                break;
+            case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_D:
+                myCar.moveRight();
+                break;
+
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
--- Project5_Cars/src/Assignment5/CarDrawer.java
+++ Project5_Cars/src/Assignment5/CarDrawer.java
@@ -12,36 +12,127 @@ import java.awt.geom.Point2D;
 
 public class CarDrawer {
 
+    //Member data
+    private int xCar, yCar;
+    private double dxCar;
+    private final int SPEED_LIMIT = 10;
+    protected Color BurntOrange = new Color(204, 102, 0);
+    private final double dt = 0.2;
+    private final double ACCELERATION = 15;
+    private final double FRICTION = 0.8;
+    private final int WIDTH_CAR = 60;
+    private final int HEIGHT_CAR = 35;
+
+    public CarDrawer() {
+        this.xCar = 100;
+        this.yCar = 100;
+        this.dxCar = 0;
+    }
+
+    public CarDrawer(int xCar, int yCar) {
+        this.xCar = xCar;
+        this.yCar = yCar;
+        this.dxCar = dxCar;
+    }
+
+    public double getDxCar() {
+        return dxCar;
+    }
+
+    public void setDxCar(double dxCar) {
+        this.dxCar = dxCar;
+    }
+
+    public int getxCar() {
+        return xCar;
+    }
+
+    public void setxCar(int xCar) {
+        this.xCar = xCar;
+    }
+
+    public int getyCar() {
+        return yCar;
+    }
+
+    public void setyCar(int yCar) {
+        this.yCar = yCar;
+    }
+
+
+    public void updateCarPosition(A5Driver display) {
+        if ( xCar + dxCar > display.getWidth() - WIDTH_CAR - 1 ) {
+            //TODO Game Over: this car is the winner.
+        }
+        if ( xCar + dxCar < WIDTH_CAR ) {
+            xCar = WIDTH_CAR;
+            dxCar = -dxCar;
+        } else
+            xCar += dxCar;
+
+        //velocity formula: i.e. v = v0 + a*t
+        dxCar += ACCELERATION * dt;
+        //Friction formula
+        dxCar *= FRICTION;
+        if ( Math.abs(dxCar) < 0.5 ) dxCar = 0;
+        //displacement formula: i.e. d = v0*t + 1/2*a*t^2
+        xCar += dxCar * dt + .5 * ACCELERATION * dt * dt;
+
+    }
+
+    /**
+     * This method paints the car on the applet screen display.
+     *
+     * @param g
+     */
     public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
 // create the car body
-        Rectangle body = new Rectangle(100, 110, 60, 10);
+        Rectangle body = new Rectangle(xCar, yCar + 10, 60, 10);
 // create the car tires
-        Ellipse2D.Double frontTire = new Ellipse2D.Double(110, 120, 10, 10);
-        g.fillOval(110, 120, 10, 10);
-        Ellipse2D.Double rearTire = new Ellipse2D.Double(140, 120, 10, 10);
+        Ellipse2D.Double frontTire = new Ellipse2D.Double(xCar + 10, yCar + 20, 10, 10);
+        g.fillOval(xCar + 10, yCar + 20, 10, 10);
+        Ellipse2D.Double rearTire = new Ellipse2D.Double(xCar + 40, yCar + 20, 10, 10);
+        g.fillOval(xCar + 40, yCar + 20, 10, 10);
+
 // create the 4 points connecting the windshields and roof
-        Point2D.Double r1 = new Point2D.Double(110, 110);
-        Point2D.Double r2 = new Point2D.Double(120, 100);
-        Point2D.Double r3 = new Point2D.Double(140, 100);
-        Point2D.Double r4 = new Point2D.Double(150, 110);
+        Point2D.Double r1 = new Point2D.Double(xCar + 10, yCar + 10);
+        Point2D.Double r2 = new Point2D.Double(xCar + 20, yCar);
+        Point2D.Double r3 = new Point2D.Double(xCar + 40, yCar);
+        Point2D.Double r4 = new Point2D.Double(xCar + 50, yCar + 10);
 // create the windshields and roof of the car
         Line2D.Double frontWindshield = new Line2D.Double(r1, r2);
         Line2D.Double roofTop = new Line2D.Double(r2, r3);
         Line2D.Double rearWindshield = new Line2D.Double(r3, r4);
 
+
 // draw all of the car parts on the screen
-        g2.draw(body);
+        g2.draw(frontWindshield);
+        g2.draw(rearWindshield);
         g2.draw(frontTire);
         g2.draw(rearTire);
-        g2.draw(frontWindshield);
+        g2.setColor(BurntOrange);
+        g2.fill(body);
         g2.draw(roofTop);
-        g2.draw(rearWindshield);
 // draw the label under the car
         g2.setColor(Color.red);
-
-
-        g2.fill3DRect(200,200, 200, 150, true);
         g2.drawString("UT JavaMobile 1.0", 100, 150);
     } // end of paint
+
+    /**
+     * This method changes dx so the car would start moving left
+     */
+    public void moveLeft() {
+        if ( Math.abs(dxCar - 3) <= SPEED_LIMIT )
+            dxCar -= 3;
+    }// end of move left
+
+    /**
+     * This method changes dx so the car would start moving right
+     */
+    public void moveRight() {
+        if ( dxCar + 3 <= SPEED_LIMIT )
+            dxCar += 3;
+    }//end of move right
+
 } // end of CarDrawer

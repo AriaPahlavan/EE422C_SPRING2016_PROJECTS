347d724b72ed44adc1a50acf8b5984f139c9498a
ariapahlavan <apahlavan1@utexas.edu>
2016-04-05 17:40:28 -0500

    All cars are now easily resizable by setting the height and  width of the car

--- Project5_Cars/src/Assignment5/CarDrawer.java
+++ Project5_Cars/src/Assignment5/CarDrawer.java
@@ -22,7 +22,7 @@ public class CarDrawer {
     private final double BACKUP_ACCELERATION = 50;
     private final double FRICTION = 0.6;
     private final int WIDTH_CAR = 60;
-    private final int HEIGHT_CAR = 35;
+    private final int HEIGHT_CAR = 32;
     private final int BACKUP_SPEED_LIMIT = 20;
     private String carNum;
 
@@ -104,18 +104,20 @@ public class CarDrawer {
     public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
 // create the car body
-        Rectangle body = new Rectangle(xCar, yCar + 10, 60, 10);
+//        RoundRectangle2D body = new RoundRectangle2D.Double(xCar, yCar + 10, 60, 15, 20, 360);
+        Rectangle body = new Rectangle(xCar,  (yCar + (5*HEIGHT_CAR)/16), WIDTH_CAR,  ((5*HEIGHT_CAR)/16));
 // create the car tires
-        Ellipse2D.Double frontTire = new Ellipse2D.Double(xCar + 10, yCar + 20, 10, 10);
-        g.fillOval(xCar + 10, yCar + 20, 10, 10);
-        Ellipse2D.Double rearTire = new Ellipse2D.Double(xCar + 40, yCar + 20, 10, 10);
-        g.fillOval(xCar + 40, yCar + 20, 10, 10);
+        Ellipse2D.Double frontTire = new Ellipse2D.Double(xCar + WIDTH_CAR / 6, yCar + (5*HEIGHT_CAR)/8,  WIDTH_CAR/5, (3 * HEIGHT_CAR)/8);
+        Ellipse2D.Double frontRim = new Ellipse2D.Double(xCar + (13 * WIDTH_CAR) / 60, yCar + (23 * HEIGHT_CAR) / 32, WIDTH_CAR/10, (3 * HEIGHT_CAR)/16);
+
+        Ellipse2D.Double rearTire = new Ellipse2D.Double(xCar + (2 * WIDTH_CAR) / 3, yCar + (5*HEIGHT_CAR)/8, 0.2 * WIDTH_CAR, (3 * HEIGHT_CAR)/8);
+        Ellipse2D.Double rearRim = new Ellipse2D.Double(xCar + (43 * WIDTH_CAR) / 60, yCar + (23 * HEIGHT_CAR) / 32, 0.1 * WIDTH_CAR, (3 * HEIGHT_CAR)/16);
 
 // create the 4 points connecting the windshields and roof
-        Point2D.Double r1 = new Point2D.Double(xCar + 10, yCar + 10);
-        Point2D.Double r2 = new Point2D.Double(xCar + 20, yCar);
-        Point2D.Double r3 = new Point2D.Double(xCar + 40, yCar);
-        Point2D.Double r4 = new Point2D.Double(xCar + 50, yCar + 10);
+        Point2D.Double r1 = new Point2D.Double(xCar + WIDTH_CAR / 6, yCar + (5*HEIGHT_CAR)/16);
+        Point2D.Double r2 = new Point2D.Double(xCar + WIDTH_CAR / 3, yCar);
+        Point2D.Double r3 = new Point2D.Double(xCar + (2 * WIDTH_CAR) / 3, yCar);
+        Point2D.Double r4 = new Point2D.Double(xCar + (5 * WIDTH_CAR) / 6, yCar + (5*HEIGHT_CAR)/16);
 // create the windshields and roof of the car
         Line2D.Double frontWindshield = new Line2D.Double(r1, r2);
         Line2D.Double roofTop = new Line2D.Double(r2, r3);
@@ -125,27 +127,30 @@ public class CarDrawer {
 // draw all of the car parts on the screen
         if ( dxCar >= 0 ) {
             g2.setColor(Color.orange);
-            g2.fillArc(xCar + 55, yCar + 13, 10, 5, 270, 180);
-        }
-        else {
+            g2.fillArc(xCar + (55 * WIDTH_CAR) / 60, yCar + (13*HEIGHT_CAR)/32, WIDTH_CAR/6, (5*HEIGHT_CAR)/32, 270, 180);
+        } else {
             g2.setColor(Color.red);
-            g2.fillArc(xCar -5, yCar + 13, 10, 5, 90, 180);
+            g2.fillArc(xCar - (5*WIDTH_CAR)/60, yCar + (13*HEIGHT_CAR)/32, WIDTH_CAR/6, (5*HEIGHT_CAR)/32, 90, 180);
         }
         g2.setColor(Color.black);
         g2.draw(frontWindshield);
         g2.draw(rearWindshield);
-        g2.draw(frontTire);
-        g2.draw(rearTire);
+        g2.fill(frontTire);
+        g2.fill(rearTire);
         g2.setColor(BurntOrange);
         g2.fill(body);
         g2.draw(roofTop);
+
+        g2.setColor(Color.gray);
+        g2.fill(frontRim);
+        g2.fill(rearRim);
 // draw the label under the car
         g2.setColor(Color.red);
-        g2.drawString("UT JavaMobile 1.0", xCar, yCar + 50);
+        g2.drawString("UT JavaMobile 1.0", xCar, yCar + (25*HEIGHT_CAR)/16);
 
         //draw the car number
         g2.setColor(Color.black);
-        g2.drawString(carNum, xCar + 30, yCar + 20);
+        g2.drawString(carNum, xCar + WIDTH_CAR/2, yCar + (5*HEIGHT_CAR)/8);
 
     } // end of paint
 
